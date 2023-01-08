8f3f7eec4cd7326b077bfb30aac455ec55baf0b3
Daniel Anderson <804021+dananderson@users.noreply.github.com>
2023-01-08 09:54:34 -0800

    Initial commit

---
+++ inc/veil.h
@@ -0,0 +1,6 @@
+/*
+ * Copyright (c) 2023 Light Source Software, LLC. All rights reserved.
+ */
+
+#pragma once
+
---
+++ src/main.c
@@ -0,0 +1,9 @@
+/* 
+ * Copyright (c) 2023 Light Source Software, LLC. All rights reserved.
+ */
+
+#include <veil.h>
+
+int main(int argc, char** argv) {
+  return 0;
+}
